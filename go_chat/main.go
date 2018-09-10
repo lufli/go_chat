9ee65e59dacbf3698d4fd5e03af4927222e23f76
Lufan <lufan-li@uiowa.edu>
2018-09-10 02:14:00 -0400

    ch 2

--- go_chat/main.go
+++ go_chat/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"go_chat/trace"
 	"html/template"
 	"log"
@@ -21,14 +26,31 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth");
+	err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("5754647965-sfh6ahbgcta78d1jb91d2cf9hm3ipe4v.apps.googleusercontent.com", "qCdUqW8_zG8o5lgZwGFgZ4NC", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("5754647965-sfh6ahbgcta78d1jb91d2cf9hm3ipe4v.apps.googleusercontent.com", "qCdUqW8_zG8o5lgZwGFgZ4NC", "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/assets", http.StripPrefix("/assets", http.FileServer(http.Dir("/path/to/assets/"))))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	go r.run()
 	log.Println("Starting web server on", *addr)
